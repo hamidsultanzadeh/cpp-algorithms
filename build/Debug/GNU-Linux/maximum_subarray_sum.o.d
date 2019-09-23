eeaa540de3bda97c61897e06285469cbdf21e8c0
hemidsultan <sultanzade.06@mail.ru>
2019-09-23 21:58:52 +0400

    first

---
+++ build/Debug/GNU-Linux/maximum_subarray_sum.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/maximum_subarray_sum.o: maximum_subarray_sum.cpp
--- maximum_subarray_sum.cpp
+++ maximum_subarray_sum.cpp
@@ -1,12 +1,13 @@
 /* 
- * File:   main.cpp
+ * File:   maximum_subarray_sum.cpp
  * Author: hamidsultanzadeh
  *
- * Created on August 27, 2019, 9:50 AM
+ * Created on September 23, 2019, 21:21
  */
 
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -14,19 +15,36 @@ int main(int argc, char** argv) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    long long n;
-    long long i = 1;
-
-    while (cin >> n && n != 0) {
-
-        double d = ceil(sqrt(9 + 8 * n));
-        cout << d << " ";
-
-        long long result = (3 + d) / 2;
-
-        cout << "Case " << i << ": " << ceil(result) << endl;
-        i++;
+    int best=0, sum=0;
+    
+    int size=0;
+    cin>>size;
+    
+    int arr[size];
+    
+    for(int i=0 ; i<size ; i++) cin>>arr[i];
+    
+    for(int i=0 ; i<size ; i++){
+        sum = max(arr[i],sum+arr[i]);
+        best = max(sum,best);
+//        cout<<sum<<"\t"<<best<<endl; DEBUG
     }
+    
+    cout<<"Best : "<<best;
 
     return 0;
 }
+//INPUT
+//8
+//-1 2 4 -3 5 2 -5 2
+
+//OUPUT
+//sum best
+//-1 0
+// 2 2
+// 6 6
+// 3 6
+// 8 8
+// 10 10
+// 5 10
+// 7 10
